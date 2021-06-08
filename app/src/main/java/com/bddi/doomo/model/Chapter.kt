b95aa6f96b543b88cfed73fc30b04c1126d8ea56
LilianMignogna <61205514+LilianMignogna@users.noreply.github.com>
2021-06-08 06:53:02 +0200

    Update inteface + writtenStoryActivity
    - Change images
    - Add icons
    - Add dynamic background images to writtenStoryActivity
    - Add sound to writtenStoryActivity

--- app/src/main/java/com/bddi/doomo/model/Chapter.kt
+++ app/src/main/java/com/bddi/doomo/model/Chapter.kt
@@ -2,5 +2,7 @@ package com.bddi.doomo.model
 
 class Chapter (
     val order: Int = 0,
-    val text: String = ""
+    val text: String = "",
+    val background_img: String = "",
+    val sound: String = ""
 )
